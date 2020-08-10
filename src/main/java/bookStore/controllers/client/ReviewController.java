b61625e81d432e75f90b27a0fedfad20f5f1a12c
Nguyen Duong <duongnguyen5566@gmail.com>
2020-08-10 09:22:07 +0700

    Duong fix setup lần 2
    Duong fix setup lần 2

---
+++ src/main/java/bookStore/controllers/client/ReviewController.java
@@ -0,0 +1,10 @@
+package bookStore.controllers.client;
+
+import org.springframework.stereotype.Controller;
+
+//Dành cho các tác vụ liên quan đến Review trong client
+
+@Controller
+public class ReviewController {
+	//Gọi hàm từ business để sử dụng
+}
