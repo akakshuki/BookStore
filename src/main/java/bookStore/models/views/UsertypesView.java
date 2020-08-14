0f70946c03fa66a1fc092d40120d3399aa56579e
Nguyen Duong <duongnguyen5566@gmail.com>
2020-08-14 07:38:13 +0700

    Duong fix setup lần 6
    Thêm ModelView

---
+++ src/main/java/bookStore/models/views/UsertypesView.java
@@ -0,0 +1,19 @@
+package bookStore.models.views;
+
+public class UsertypesView {
+	private int Idut;
+	private String NameUT;
+	public int getIdut() {
+		return Idut;
+	}
+	public void setIdut(int idut) {
+		Idut = idut;
+	}
+	public String getNameUT() {
+		return NameUT;
+	}
+	public void setNameUT(String nameUT) {
+		NameUT = nameUT;
+	}
+
+}
